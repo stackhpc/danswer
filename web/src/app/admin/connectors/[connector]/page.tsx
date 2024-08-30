fa448ce97658b022725714de255129ff4711b861
sd109 <sdavidson327@gmail.com>
2024-08-30 14:25:45 +0100

    Remove unused files from helm-publish branch

--- web/src/app/admin/connectors/[connector]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ConnectorWrapper from "./ConnectorWrapper";
-
-export default async function Page({
-  params,
-}: {
-  params: { connector: string };
-}) {
-  return <ConnectorWrapper connector={params.connector.replace("-", "_")} />;
-}
--- web/src/components/search/DocumentUpdatedAtBadge.tsx
+++
@@ -1,6 +0,0 @@
-import { timeAgo } from "@/lib/time";
-import { MetadataBadge } from "../MetadataBadge";
-
-export function DocumentUpdatedAtBadge({ updatedAt }: { updatedAt: string }) {
-  return <MetadataBadge value={"Updated " + timeAgo(updatedAt)} />;
-}
